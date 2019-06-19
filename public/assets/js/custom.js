e744b2e8ee505bc2aada34a3a20ed78a2fef295c
kirti <kirti@intellarc.com>
2019-06-19 15:19:41 +0530

    added css for product-view page

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -80,7 +80,7 @@
 	var newHeight = $(".slider-for").height();
 	$('.slick-vertical .slick-slide').height(function(){ return newHeight/3; });
 	$(".slick-slide").css("overflow","hidden");
-	$(".slick-slide img").css("width","100%");
+	$(".slider-nav .slick-slide img").css("width","100%");
 	$(".slick-slide img").css("margin","auto");
 
 	// ZOOM
