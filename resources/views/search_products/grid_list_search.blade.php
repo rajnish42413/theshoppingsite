553758b413c64780c5715afae37d7789c02577b1
kirti <kirti@intellarc.com>
2019-07-08 17:05:56 +0530

    Solved #8

--- resources/views/search_products/grid_list_search.blade.php
+++ resources/views/search_products/grid_list_search.blade.php
@@ -150,11 +150,11 @@
 										<div class="out_of_stock">
 											<span>Out of Stock</span>
 										</div>
-									<?php }elseif($product->Quantity <= 10){?>
+									<?php }//elseif($product->Quantity <= 10){?>
 										<!--<div class="less_stock">
 											<span>Only <?php //echo $product->Quantity;?> Left</span>
 										</div>-->									
-									<?php }?>
+									<?php //}?>
 										<img class="grid_prd" src="<?php echo $galleryURL;?>">
 										<h4 class="sh_prod_name"><?php echo $title;?></h4>
 										<div class="sh_about_prod">
